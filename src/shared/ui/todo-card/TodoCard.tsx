e4df9dd8ee1ad46faf36d8f7992e6639abf723bc
dmitrykezarev <dkezarev516@gmail.com>
2025-05-09 00:51:41 +0500

    Дорабатывает карточку todo

--- src/shared/ui/todo-card/TodoCard.tsx
+++ src/shared/ui/todo-card/TodoCard.tsx
@@ -1,16 +1,19 @@
 import styles from './TodoCard.module.scss';
-import Button from '../button/Button';
 import DeleteSvg from '../../../shared/assets/svg/delete.svg';
-import EditSvg from '../../../shared/assets/svg/edit.svg';
 import TimeSvg from '../../assets/svg/time.svg';
 import ITodo from '../../../entities/models/ITodo';
 import { DateUtils } from '../../utils/Date';
-import Priority from '../../../entities/models/Priority';
 import { useNavigate } from 'react-router-dom';
 import { useAppDispatch } from '../../../store/types/useAppDispatch';
 import { Status } from '../../../entities/models/TodoStatus';
 import { updateTodoAsync } from '../../../store/services/todo-list/slice/updateTodoAsync';
 import { PriorityUtils } from '../../utils/Priority';
+import { useState } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
+import LowIcon from '../../assets/svg/lowSuccess.svg';
+import MediumIcon from '../../assets/svg/mediumSuccess.svg';
+import HighIcon from '../../assets/svg/highSuccess.svg';
+import Priority from '../../../entities/models/Priority';
 
 export enum TodoCardStatus {
 	DEFAULT = 'DEFAULT',
@@ -30,42 +33,145 @@ const TodoCard = (props: ITodoCardProps) => {
 	} = props;
 	const navigate = useNavigate();
 	const dispatch = useAppDispatch();
+	const [isEnter, setIsEnter] = useState(false);
 
 	const handleEditTodo = () => {
 		navigate(`edit/${todo.id}`);
 	};
 	
-	const handleCompleteTodo = () => {
-		const newTodo: ITodo = {
-			...todo,
-			status: Status.COMPLETED,
-		};
-		dispatch(updateTodoAsync(newTodo));
+	const handleCompleteTodo = (event: React.MouseEvent<HTMLDivElement>) => {
+		event.stopPropagation();
+
+		const result = confirm(`Вы уверены, что хотите Выполнить задачу? \n\n${todo.title}`);
+		if (result) {
+			const newTodo: ITodo = {
+				...todo,
+				status: Status.COMPLETED,
+			};
+			dispatch(updateTodoAsync(newTodo));
+		} else {
+			// Cancel
+		}
 	};
 
-	const handleDeleteTodo = () => {
-		const newTodo: ITodo = {
-			...todo,
-			isDeleted: true,
-		};
-		dispatch(updateTodoAsync(newTodo));
+	const handleDeleteTodo = (event: React.MouseEvent<HTMLDivElement>) => {
+		event.stopPropagation();
+
+		const result = confirm(`Вы уверены, что хотите Удалить задачу? \n\n${todo.title}`);
+		if (result) {
+			const newTodo: ITodo = {
+				...todo,
+				isDeleted: true,
+			};
+			dispatch(updateTodoAsync(newTodo));
+		} else {
+			// Cancel
+		}
 	};
 
-	return (
-		<div className={styles.TodoCard}>
-			<div className={styles.TodoCardMain}>
-				<div>
+	const handleMouseEnter = () => {
+		setIsEnter(true);
+	};
+
+	const handleMouseLeave = () => {
+		setIsEnter(false);
+	};
+
+	const imgContent = () => {
+		if (status === TodoCardStatus.DEFAULT) {
+			if (isEnter) {
+				switch (todo.priority) {
+				case Priority.LOW:
+					return <img src={LowIcon} alt="low" onClick={handleCompleteTodo} style={{ cursor: 'pointer' }} />;
+				case Priority.MEDIUM:
+					return <img src={MediumIcon}
+						alt="medium" onClick={handleCompleteTodo} style={{ cursor: 'pointer' }} />;
+				case Priority.HIGH:
+					return <img src={HighIcon} alt="high" onClick={handleCompleteTodo} style={{ cursor: 'pointer' }} />;
+				default:
+					return <img src={LowIcon} alt="low" onClick={handleCompleteTodo} style={{ cursor: 'pointer' }} />;
+				}
+			} else {
+				return (
 					<div
 						className={styles.TodoCardMainButton}
 						style={{ backgroundColor: PriorityUtils.priorityToColor(todo.priority) }}
 					>
 					</div>
+				);
+			}
+		} else if (status === TodoCardStatus.COMPLETED) {
+			switch (todo.priority) {
+			case Priority.LOW:
+				return <img src={LowIcon} alt="low" />;
+			case Priority.MEDIUM:
+				return <img src={MediumIcon} alt="medium" />;
+			case Priority.HIGH:
+				return <img src={HighIcon} alt="high" />;
+			default:
+				return <img src={LowIcon} alt="low" />;
+			}
+		} else if (status === TodoCardStatus.DELETED) {
+			return (
+				<img
+					src={DeleteSvg}
+					alt="deleteTodo"/>
+			);
+		}
+	};
+
+	return (
+		<div
+			className={styles.TodoCard}
+			onMouseEnter={handleMouseEnter}
+			onMouseLeave={handleMouseLeave}
+			onClick={handleEditTodo}
+		>
+			<div className={styles.TodoCardMain}>
+				<div>
+					{imgContent()}
 				</div>
 
 				<span>
 					{todo.title}
 				</span>
+
+				{
+					status === TodoCardStatus.DEFAULT && (
+						<AnimatePresence>
+							{isEnter && (
+								<motion.div
+									initial={{ opacity: 0 }}
+									animate={{ opacity: 1 }}
+									exit={{ opacity: 0 }}
+									transition={{ duration: 0.3 }}
+									className={styles.TodoCardDelete}
+								>
+									<img
+										className={styles.TodoCardDelete}
+										src={DeleteSvg}
+										alt="deleteTodo"
+										onClick={handleDeleteTodo}/>
+								</motion.div>
+							)}
+						</AnimatePresence>
+					)
+				}
 			</div>
+
+			<div className={styles.TodoCardMainDescription}>
+				<span>
+					{todo.description}
+				</span>
+			</div>
+
+			<div className={styles.TodoCardMainTime}>
+				<img src={TimeSvg}/>
+				<span>
+					{DateUtils.formatUTCToRussian(todo.dateCompleted)}
+				</span>
+			</div>
+
 			{/*<div className={styles.TodoCardMain}>*/}
 			{/*	{status === TodoCardStatus.DEFAULT && (*/}
 			{/*		<Button onClick={handleCompleteTodo} size="small" className={styles.TodoCardMainButton}>*/}
